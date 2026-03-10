9637a2f7d35da2c85bdfcf15f2264bcd870b631f
sk <sofkvvv@email.com>
2026-03-10 10:27:14 +0300

    added go and config files for golangci

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,10 +1,19 @@
+//go:build golangci
+
 package main
 
 import (
 	"github.com/golangci/plugin-module-register/register"
 	"github.com/sofk69/loglint/pkg/analyzer"
+	"golang.org/x/tools/go/analysis"
 )
 
 func init() {
-	register.Plugin("loglint", analyzer.Analyzer)
+	register.Plugin("loglint", New)
+}
+
+func New() ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{
+		analyzer.Analyzer,
+	}, nil
 }
