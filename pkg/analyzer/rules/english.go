2660d147b4087fa9360bb9e09802885f698c094e
sk <sofkvvv@email.com>
2026-03-10 14:40:11 +0300

    modified

--- pkg/analyzer/rules/english.go
+++ pkg/analyzer/rules/english.go
@@ -1,14 +1,13 @@
 package rules
 
 func IsEnglish(msg string) bool {
-
 	for _, r := range msg {
-
-		if r > 127 {
+		if !((r >= 'a' && r <= 'z') ||
+			(r >= 'A' && r <= 'Z') ||
+			(r >= '0' && r <= '9') ||
+			r == ' ' || r == '.' || r == ',') {
 			return false
 		}
-
 	}
-
 	return true
 }
