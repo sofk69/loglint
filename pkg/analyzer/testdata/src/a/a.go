7936c3ddb9b83e986fbf626d23e313b27c3091f8
sk <sofkvvv@email.com>
2026-03-10 16:31:46 +0300

    small changes

--- pkg/analyzer/testdata/src/a/a.go
+++ pkg/analyzer/testdata/src/a/a.go
@@ -6,9 +6,13 @@ import (
 
 func test() {
 	slog.Info("Starting server") // want "log message should start with lowercase"
-	slog.Info("запуск сервера")  // want "log message must be english"
+
+	slog.Info("запуск сервера") // want "log message must be english"
+	// "log message contains special characters"
+
 	slog.Info("server started!") // want "log message contains special characters"
 
 	password := "123"
-	slog.Info("user password: " + password) // want "log message must be english" "log message may contain sensitive data"
+	slog.Info("user password: " + password) // want "log message must be english"
+	// "log message may contain sensitive data"
 }
